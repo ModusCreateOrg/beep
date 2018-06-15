c9ec6f6747a70d94f2934f40b2e59b9d5d91fedb
Michael Tintiuc <contact@michaeltintiuc.com>
2018-06-15 19:31:52 +0300

    cleanup deployment scripts

--- build/deploy-to-gh-pages.sh
+++ build/deploy-to-gh-pages.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
-set -o
-
 BRANCH='gh-pages'
 DIR='dist'
 
 if [ "$(git status --porcelain)" ];
 then
   git status
-  echo "Deployment failed!"
+  echo "Some pending changes require your attention"
   exit 1;
 fi
 
-git subtree split --prefix ${DIR} -b ${BRANCH}
+git subtree split -P ${DIR} -b ${BRANCH}
 git push -f origin ${BRANCH}:${BRANCH}
 git branch -D ${BRANCH}
