0c6a47f2d80d21a4a0372eb338d8b70bedecc9f1
Michael Tintiuc <contact@michaeltintiuc.com>
2018-06-21 12:30:27 +0300

    Allow for reverse ionic transitions when going back in history

--- src/IonRouterView.vue
+++ src/IonRouterView.vue
@@ -1,6 +1,17 @@
 <template>
     <ion-router-outlet>
-        <transition v-bind:css="false" v-on:before-enter="beforeEnter" v-on:before-leave="beforeLeave">
+        <transition
+            mode="in-out"
+            v-bind:css="false"
+            v-on:before-enter="beforeEnter"
+            v-on:enter="enter"
+            v-on:after-enter="afterEnter"
+            v-on:enter-cancelled="enterCancelled"
+            v-on:before-leave="beforeLeave"
+            v-on:leave="leave"
+            v-on:after-leave="afterLeave"
+            v-on:leave-cancelled="leaveCancelled"
+        >
             <router-view></router-view>
         </transition>
     </ion-router-outlet>
@@ -11,25 +22,29 @@ export default {
     name: 'IonRouterView',
     data() {
         return {
-            views: 0,
             animated: true,
             enteringEl: null,
+            leavingEl: null,
         }
     },
     methods: {
         beforeEnter(element) {
-            if (this.animated) {
-                element.style.opacity = 0
-            }
             this.enteringEl = element
         },
+        enter(element, done) {
+            done()
+        },
+        afterEnter(el) {},
+        enterCancelled(el) {},
         beforeLeave(element) {
-            this.transition(this.enteringEl, element)
-            this.views++
+            this.leavingEl = element
         },
-        canGoBack() {
-            return this.views > 0 && this.$router.currentRoute.path.length > 1
+        leave(element, done) {
+            element.style.opacity=0
+            this.transition(this.enteringEl, element).then(() => done())
         },
+        afterLeave(el) {},
+        leaveCancelled(el) {},
         async transition(enteringEl, leavingEl) {
             const ionRouter = document.querySelector('ion-router-outlet')
 
@@ -40,7 +55,9 @@ export default {
             await ionRouter.componentOnReady()
             await ionRouter.commit(enteringEl, leavingEl, {
                 duration: !this.animated ? 0 : undefined,
-                showGoBack: this.canGoBack(),
+                direction: this.$router.direction === 1 ? 'forward' : 'back',
+                deepWait: true,
+                showGoBack: this.$router.canGoBack(),
             })
         },
     },
--- src/router.js
+++ src/router.js
@@ -5,6 +5,22 @@ import IonRouterView from './IonRouterView.vue'
 export default class Router extends VueRouter {
     constructor(...args) {
         super(...args)
+        this.direction = args.direction || 1
+        this.viewCount = args.viewCount || 0
+    }
+    push(...args) {
+        super.push(...args)
+        this.direction = 1
+        this.viewCount++
+    }
+    go(n) {
+        super.go(n)
+        this.viewCount += n
+        this.direction = n > 0 ? 1 : -1
+    }
+    canGoBack() {
+        // TODO: hook into history back event and -1 viewCount
+        return this.viewCount > 0 && this.currentRoute.path.length > 1
     }
 }
 
