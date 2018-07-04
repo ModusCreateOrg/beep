b2c26369f47b4204363a0a39f501cc6f1ffb0ef3
Michael Tintiuc <contact@michaeltintiuc.com>
2018-07-04 11:43:10 +0300

    Allow users to toggle Vuejs transition CSS classes

--- src/IonRouterView.vue
+++ src/IonRouterView.vue
@@ -2,7 +2,7 @@
     <ion-router-outlet ref="ionRouterOutlet" @click="catchIonicGoBack">
         <transition
             mode="in-out"
-            v-bind:css="false"
+            v-bind:css="bindCss"
             v-on:before-enter="beforeEnter"
             v-on:enter="enter"
             v-on:after-enter="afterEnter"
@@ -27,6 +27,10 @@ export default {
         }
     },
     props: {
+        bindCss: {
+            type: Boolean,
+            default: false,
+        },
         animated: {
             type: Boolean,
             default: true,
@@ -75,17 +79,11 @@ export default {
         },
         beforeLeave(element) {
             this.leavingEl = element
-
-            if (!this.enteringEl || !this.animated || this.$router.direction < 0) {
-                return;
-            }
         },
         leave(element, done) {
-            if (!this.animated) {
-                return done()
+            if (this.animated) {
+                this.transition(this.enteringEl, element).then(() => done())
             }
-
-            this.transition(this.enteringEl, element).then(() => done())
         },
         enter(element, done) {
             done()
