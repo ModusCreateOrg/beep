0c119456600cffd3394a41083ed4cb7d3bad5401
Grgur Grisogono <grgur@grgur.com>
2018-08-07 10:40:13 +0200

    Re-introduced plugin:promise/recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
-    '@vue/prettier'
+    '@vue/prettier',
+    'eslint:recommended',
+    'plugin:promise/recommended',
   ],
+  plugins: ['vue', 'promise'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
+    parser: 'babel-eslint',
+  },
 }
