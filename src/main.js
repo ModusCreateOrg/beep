cf4d1e3526d71e9b4af7783dde19b9afc363b115
Michael Tintiuc <contact@michaeltintiuc.com>
2018-08-18 01:41:01 +0300

    Defer all ionic init code to ionic-vue lib

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 import Vue from 'vue'
-import '@ionic/core/css/ionic.css'
 import './theme/common.css'
 import { IonicAPI } from '@modus/ionic-vue'
 import router from './router'
 import './registerServiceWorker'
 import BreachService from './breachesService'
-
-import '@ionic/core/dist/ionic/svg/md-arrow-back.svg'
-
-import { defineCustomElements } from '@ionic/core/dist/esm'
-defineCustomElements(window, { resourcesUrl: 'img/' })
 
 Vue.config.productionTip = false
 
