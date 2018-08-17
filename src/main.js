6c96360e1e24d9ac1aafb71c358220d9a18fbf3b
Michael Tintiuc <contact@michaeltintiuc.com>
2018-08-17 23:19:48 +0300

    Import icon test

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import router from './router'
 import './registerServiceWorker'
 import BreachService from './breachesService'
 
+import '@ionic/core/dist/ionic/svg/md-arrow-back.svg'
+
 import { defineCustomElements } from '@ionic/core/dist/esm'
-defineCustomElements(window)
+defineCustomElements(window, { resourcesUrl: 'img/' })
 
 Vue.config.productionTip = false
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   runtimeCompiler: true,
+  configureWebpack: config => ({
+      module: {
+        rules: [{
+          test: /\.(svg)(\?.*)?$/,
+            loader: 'file-loader',
+            options: {
+              name: 'img/svg/[name].[ext]',
+            },
+        }]
+      }
+  })
 }
