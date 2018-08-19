591060b7462b1f80a5ea935a41f37fab46ef3626
Michael Tintiuc <contact@michaeltintiuc.com>
2018-08-19 22:22:53 +0300

    Add PWA config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   baseUrl: '/',
   runtimeCompiler: true,
+  pwa: {
+    themeColor: "#FFFFFF",
+    msTileColor: "#FFFFFF",
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'white',
+  }
 }
