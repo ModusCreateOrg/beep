fd14396478ae3d86ac7f8d6295d350473081b771
Michael Tintiuc <contact@michaeltintiuc.com>
2018-08-21 00:38:06 +0300

    Fix pwa name, use env consts in vue config

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,9 @@ module.exports = {
   baseUrl: '/',
   runtimeCompiler: true,
   pwa: {
-    themeColor: '#FFFFFF',
-    msTileColor: '#FFFFFF',
+    name: process.env.VUE_APP_NAME,
+    themeColor: process.env.VUE_APP_INITIAL_STATUSBAR_COLOR,
+    msTileColor: process.env.VUE_APP_INITIAL_STATUSBAR_COLOR,
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black-translucent',
   },
