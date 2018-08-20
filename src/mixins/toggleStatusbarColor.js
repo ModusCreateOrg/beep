9bffb292b8ab0f75708bd94587828a7abf5e25b7
Michael Tintiuc <contact@michaeltintiuc.com>
2018-08-20 03:58:52 +0300

    Add native statusbar switching

--- src/mixins/toggleStatusbarColor.js
+++ src/mixins/toggleStatusbarColor.js
@@ -1,3 +1,6 @@
+import { Plugins } from '@capacitor/core'
+const { StatusBar } = Plugins
+
 export default {
   data() {
     return {
@@ -8,10 +11,18 @@ export default {
   mounted() {
     document.querySelector('meta[name="theme-color"]').content = this.newStatusbarColor
     document.querySelector('meta[name="msapplication-TileColor"]').content = this.newStatusbarColor
+    StatusBar.setBackgroundColor({ color: this.newStatusbarColor }).catch(() =>
+      console.log('Statusbar is not available')
+    )
   },
   beforeRouteLeave(to, from, next) {
     document.querySelector('meta[name="theme-color"]').content = this.initialStatusbarColor
-    document.querySelector('meta[name="msapplication-TileColor"]').content = this.initialStatusbarColor
+    document.querySelector(
+      'meta[name="msapplication-TileColor"]'
+    ).content = this.initialStatusbarColor
+    StatusBar.setBackgroundColor({ color: this.initialStatusbarColor }).catch(() =>
+      console.log('Statusbar is not available')
+    )
     next()
   },
 }
