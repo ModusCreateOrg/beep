e0f732c6f662d8d152ab80d05a4dd48826db10c2
Ernesto Ramírez <ernesto.ramirez88@gmail.com>
2018-09-26 23:55:16 -0700

    Fixed iPhone XR support (#106)

--- src/views/Breaches.vue
+++ src/views/Breaches.vue
@@ -111,9 +111,12 @@ ion-back-button {
   padding: 7px 10px;
 }
 
-/* iPhone X, Xs, X-MAX and XR*/
+/* iPhone support
+ * first selector: X/Xs/X-MAX 
+ * second selector: XR
+ */
 @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape),
-  only screen and (min-device-width: 414px) and (max-device-width: 896px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+  only screen and (min-device-width: 414px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
   ion-list,
   ion-searchbar {
     padding-right: env(safe-area-inset-right);
