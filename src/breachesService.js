c47af7d34e9cd6c119089c012b896e98ae764a78
Michael <michaelwbonner@gmail.com>
2018-12-12 09:23:11 -0700

    use LogoPath from haveibeenpwned API (#120)

--- src/breachesService.js
+++ src/breachesService.js
@@ -2,15 +2,11 @@ export default {
   breaches: [],
   filter: '',
   account: '',
-  baseImageURL: 'https://haveibeenpwned.com/Content/Images/PwnedLogos/',
   baseApiURL: 'https://haveibeenpwned.com/api/v2/breachedaccount/',
   clear() {
     this.breaches = []
     this.filter = ''
     this.account = ''
-  },
-  getImageURL(breach) {
-    return this.baseImageURL + breach.Name + '.' + breach.LogoType
   },
   formatDomain(domain) {
     return domain.startsWith('www') ? domain : `www.${domain}`
