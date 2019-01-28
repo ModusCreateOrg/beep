a23913a2666fa90f04d63b40e1722e42752997b6
Sergiu Bacanu <sergiu.bacanu@yahoo.com>
2019-01-28 17:52:46 +0200

    Added docker scripts. Updated CI config.

---
+++ tests_root/wdio/configs/wdio.conf.grid.parallel.js
@@ -0,0 +1,25 @@
+const configBase = require('./wdio.conf');
+
+exports.config = Object.assign({}, configBase.config, {
+  host: 'localhost',
+  port: 4444,
+  path: '/wd/hub',
+  protocol: 'http',
+  maxInstances: 1,
+  capabilities: [
+    {
+      maxInstances: 1,
+      browserName: 'firefox',
+      specs: [],
+      'moz:firefoxOptions': {
+        // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
+        // args: ['-headless']
+      },
+    },
+    {
+      maxInstances: 1,
+      browserName: 'chrome',
+      chromeOptions: {},
+    },
+  ],
+});
