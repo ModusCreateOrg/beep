924edf25d5f6cdb6bcb273f190862aeb101570fe
Sergiu Bacanu <sergiu.bacanu@yahoo.com>
2019-01-29 16:19:27 +0200

    Updated host in wdio config.

--- tests_root/wdio/configs/wdio.conf.grid.parallel.js
+++ tests_root/wdio/configs/wdio.conf.grid.parallel.js
@@ -1,7 +1,7 @@
 const configBase = require('./wdio.conf');
 
 exports.config = Object.assign({}, configBase.config, {
-  host: '0.0.0.0',
+  host: 'hub',
   port: 4444,
   path: '/wd/hub',
   protocol: 'http',
