6c825b161050857d1fc178a0fc0a331925c29dd9
Michael Tintiuc <contact@michaeltintiuc.com>
2019-02-01 18:46:30 +0200

    Proxy requests to HIBP

---
+++ src/proxy.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const request = require('request')
+
+const app = express()
+const port = process.env.PROXY_PORT || 5000
+
+app.listen(port, () => console.log(`[BEEP] Proxy server listening on port ${port}\n`))
+
+app.use('/', function(req, res) {
+  const proxy = request({
+    url: `https://${req.url.substr(1)}`,
+    headers: { 'User-Agent': 'Beep' },
+  })
+
+  req.pipe(proxy).pipe(res)
+})
